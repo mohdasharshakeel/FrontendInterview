2c643b2271f47ee232d4928045839d8d9a5d8aa8
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-15 16:56:11 +0530

    hnn started

--- javascript04.js
+++ javascript04.js
@@ -1,9 +1,20 @@
-let n = 5 ;
-let res = n++ + ++n ;
-console.log(res )
+// let n = 5 ;
+// let res = n++ + ++n ;
+// console.log(res )
 
-let k = 5 
-let res1  = ++k + ++k 
-console.log(res1) // 13 
+// let k = 5 
+// let res1  = ++k + ++k 
+// console.log(res1) // 13 
+
+///! deep copy and shallow copy
+let obj = {
+    name : "ashar",
+    age : 20,
+    address : {
+        city : "soul socity"
+    }
+}
+
+let obj2 = JSON.parse(JSON.stringify(obj)) // deep copy 
 
 
